b7b9a3aecee5613ed6477540bbcefa2e79b20810
unknown-29 <107143447+unknown-29@users.noreply.github.com>
2023-10-23 10:44:58 +0530

    Create naive_string_matching.cpp

---
+++ naive_string_matching.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void search(string& pat, string txt)
+{
+	int M = pat.size();
+	int N = txt.size();
+	for (int i = 0; i <= N - M; i++) {
+		int j;
+
+		for (j = 0; j < M; j++)
+			if (txt[i + j] != pat[j])
+				break;
+
+		if (j == M)
+			cout << "Pattern found at index " << i << endl;
+	}
+}
+int main()
+{
+	string txt = "AABAACAADAABAAABAA";
+	string pat = "AABA";
+	search(pat, txt);
+	return 0;
+}
